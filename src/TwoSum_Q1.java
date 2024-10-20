eb6fc75adf49e934e27fb16d04d4019f2c69d9d7
abinash_dash <abinash3398@gmail.com>
2024-10-20 23:03:05 +0530

    version1

---
+++ src/TwoSum_Q1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class TwoSum_Q1 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int index = 0;
+            while (index < nums.length) {
+                if (index != i) {
+                    if (nums[i] + nums[index] == target) {
+                        return new int[]{i, index};
+                    }
+                }
+                index++;
+            }
+        }
+        return null;
+    }
+}
